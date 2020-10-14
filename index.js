d56287d7e2877cbf1dd12d037e3b4a03b8d4d4bd
unknown <Katherine2bk@gmail.com>
2020-10-14 12:53:10 -0700

    set up server in index.js... setting up router for posts after this, will insert router into index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// require cors here if needed
+// need postsRouter
+const server = express();
+server.use(express.json());
+// this is where you put server.use(cors());
+
+// server.use(...postsRouter)
+
+const port = 8000;
+
+server.get('/', (req, res) => {
+    res.send('Welcome to the API')
+})
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
+
