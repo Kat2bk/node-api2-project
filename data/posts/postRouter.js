12d3decc5826dcf187fb96cc7ee9f92e7ba92c19
unknown <Katherine2bk@gmail.com>
2020-10-14 13:53:13 -0700

    added router to index.js... created three new get requests in postRouter... commented out completed requests in README file

--- data/posts/postRouter.js
+++ data/posts/postRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Posts = require('../db');
+const { json } = require('express');
 
 const router = express.Router();
 
@@ -16,4 +17,51 @@ router.get('/', (req, res) => {
         });
     });
 });
+
+// get post by id 
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+
+    Posts.findById(id)
+    .then(post => {
+        if (!post) {
+            json.status(404).json({
+                message: "The post with the specified ID does not exist." 
+            })
+        } else {
+            res.status(200).json(post)
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({
+            error: "The post info could not be retrieved"
+        })
+    })
+})
+
+router.get('/:id/comments', (req, res) => {
+    const {id} = req.params;
+
+    Posts.findCommentById(id)
+    .then(comment => {
+        if (!comment) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            })
+        } else {
+            res.status(200).json(comment);
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({
+            message: "The comments info could not be retrieved"
+        })
+    })
+})
+
+
+module.exports = router;
 
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 // require cors here if needed
 // need postsRouter
+const postsRouter = require('./data/posts/postRouter');
 const server = express();
 server.use(express.json());
 // this is where you put server.use(cors());
 
 // server.use(...postsRouter)
+server.use('/api/posts', postsRouter);
 
 const port = 5000;
 
