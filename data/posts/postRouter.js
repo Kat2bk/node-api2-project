d111f7db31c0fc472cb20cdba14a4433c60606ac
unknown <Katherine2bk@gmail.com>
2020-10-14 14:01:14 -0700

    server.post request works, tested in Insomnia, continuing with post request for comments

--- data/posts/postRouter.js
+++ data/posts/postRouter.js
@@ -62,6 +62,30 @@ router.get('/:id/comments', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    const postInfo = {
+        title: req.body.title,
+        contents: req.body.contents
+    }
+
+    if (!postInfo.title || !postInfo.contents) {
+       return res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+        })
+    } else {
+        Posts.insert(postInfo)
+        .then(post => {
+            res.status(201).json(post)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({
+                errorMessage: "There was an error while saving the post to the database"
+            })
+        })
+    }
+})
+
 
 module.exports = router;
 
